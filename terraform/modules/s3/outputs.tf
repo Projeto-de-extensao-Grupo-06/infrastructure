ed705972987346ffda7d882684d933d91f3779d7
RanierCouto <ranier.couto@exedconsulting.com>
2026-03-04 17:40:18 -0300

    chore: removendo arquivos grandes de estado e lock do terraform

--- terraform/modules/s3/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "bucket_id" {
-  description = "ID/Name do bucket S3"
-  value       = aws_s3_bucket.this.id
-}
-
-output "bucket_arn" {
-  description = "ARN do bucket S3"
-  value       = aws_s3_bucket.this.arn
-}
--- terraform/modules/s3/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "environment" {
-  description = "Ambiente (dev, prod)"
-  type        = string
-}
-
-variable "bucket_name" {
-  description = "Nome base do bucket"
-  type        = string
-}
